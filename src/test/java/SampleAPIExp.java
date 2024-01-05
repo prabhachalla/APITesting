603bc39c45873b0d422322b7e47eee2b6a9d6f82
Prabhakar Reddy <prabhachalla1993@gmail.com>
2024-01-05 16:16:15 +0530

    API Testing

---
+++ src/test/java/SampleAPIExp.java
@@ -0,0 +1,20 @@
+import io.restassured.RestAssured;
+import org.testng.annotations.Test;
+
+public class SampleAPIExp {
+
+
+    @Test(description = "example for static import", enabled = true)
+    public void staticImportExample(){
+
+        RestAssured.given().
+                baseUri("https://api.postman.com").
+                header("X-Api-Key","PMAK-6597a145fd32020038584c7b-2a08687a94858176df74573cf4f1ed2d84").
+                log().all().
+                when().
+                get("/workspaces").
+                then().
+                log().all();
+    }
+
+}
